fe0774dd31755bbb532f2f6b72388119778ceb7f
Elbert  Ribeiro <elbertprofissional@gmail.com>
2023-04-06 23:17:31 -0300

    renomeando pacote

--- src/main/java/com/elbertribeiro/BookApi.java
+++ src/main/java/com/elbertribeiro/BookApi.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.elbertribeiro;
 
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
--- src/test/java/com/elbertribeiro/BookApiTests.java
+++ src/test/java/com/elbertribeiro/BookApiTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.elbertribeiro;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
