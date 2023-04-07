b498d2cf85660b3a69dc7e98747e4e9f7b2fe5c2
Elbert  Ribeiro <elbertprofissional@gmail.com>
2023-04-06 23:18:56 -0300

    test: refatorando testes

--- src/test/java/com/elbertribeiro/BookApiTests.java
+++ src/test/java/com/elbertribeiro/BookApiTests.java
@@ -1,28 +1,22 @@
 package com.elbertribeiro;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.junit4.SpringRunner;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-public class BookApiTests {
+class BookApiTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private TestRestTemplate restTemplate;
 
-	@Autowired
-	private TestRestTemplate restTemplate;
-
-	@Test
-	public void homeResponse() {
-		String body = this.restTemplate.getForObject("/", String.class);
-		assertThat(body).isEqualTo("Spring is here!");
-	}
+    @Test
+    void homeResponse() {
+        String body = this.restTemplate.getForObject("/", String.class);
+        assertThat(body).isEqualTo("Spring is here!");
+    }
 }
