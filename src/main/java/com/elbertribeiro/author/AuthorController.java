a1ae2c1fe29e152c6c04714e22ec6e736ab3a899
Elbert  Ribeiro <elbertprofissional@gmail.com>
2023-04-08 00:40:48 -0300

    feat: adicionando mutations ao projeto

--- src/main/java/com/elbertribeiro/author/AuthorController.java
+++ src/main/java/com/elbertribeiro/author/AuthorController.java
@@ -1,7 +1,7 @@
 package com.elbertribeiro.author;
 
 import org.springframework.graphql.data.method.annotation.Argument;
-import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,7 +12,7 @@ public class AuthorController {
         this.service = service;
     }
 
-    @QueryMapping
+    @MutationMapping
     public Author saveAuthor(@Argument AuthorDto author) {
         return service.saveAuthor(author.name(), author.email());
     }
