8806cf180ea0ddb762aff6a0d6d6bb3286a9edd3
Elbert  Ribeiro <elbertprofissional@gmail.com>
2023-04-09 13:51:54 -0300

    feat: adicionando a lista de livros

--- src/main/kotlin/com/elbertribeiro/book/BookController.kt
+++ src/main/kotlin/com/elbertribeiro/book/BookController.kt
@@ -2,10 +2,16 @@ package com.elbertribeiro.book
 
 import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.MutationMapping
+import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class BookController(val service: BookService) {
+
+    @QueryMapping
+    fun listBook(): List<BookDto> {
+        return service.list().map { book -> book.toBookDto() }
+    }
 
     @MutationMapping
     fun saveBook(@Argument book: BookDto): BookDto {
--- src/main/kotlin/com/elbertribeiro/book/BookService.kt
+++ src/main/kotlin/com/elbertribeiro/book/BookService.kt
@@ -4,6 +4,10 @@ import org.springframework.stereotype.Service
 
 @Service
 class BookService(val repository: BookRepository) {
+    fun list() : List<Book> {
+        return repository.findAll()
+    }
+
     fun save(title: String) : Book {
         return repository.save(Book(title))
     }
