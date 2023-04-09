96a0c5979a1c8253e468488fbe8d59ff7e5d195b
Elbert  Ribeiro <elbertprofissional@gmail.com>
2023-04-08 23:41:11 -0300

    feat: atualizando docs

--- src/main/kotlin/com/elbertribeiro/book/BookRepository.kt
+++ src/main/kotlin/com/elbertribeiro/book/BookRepository.kt
@@ -4,5 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface BookRepository : JpaRepository<Book, Long> {
-}
+interface BookRepository : JpaRepository<Book, Long>
